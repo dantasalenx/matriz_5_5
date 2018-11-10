4b6e57d2405cb6e2ffa9522cddee16684cb0ec20
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2018-11-09 20:53:29 -0400

    matriz 2 por 2

---
+++ matriz5_5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace matriz5_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o numero a ser verificado: ");
+            int numero = Int32.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[2, 2];
+
+            for (int linha = 0; linha < 2; linha++)
+            {  
+                for (int coluna = 0; coluna < 2; coluna++)
+                {
+                    Console.WriteLine("Informe um numero: ");
+                    matriz[linha, coluna] = Int32.Parse(Console.ReadLine());
+                }
+            }
+
+
+            int contador = 0; 
+            for (int linha = 0; linha < 2; linha++)
+            {
+                for (int coluna = 0; coluna < 2; coluna++)
+                {
+                    if (numero == matriz[linha, coluna]) contador++;
+                }
+            }
+            Console.WriteLine("O numero {0} repetiu {1} vezes", numero, contador);
+
+
+        }
+    }
+}
