30babc66bc1a90ae564a9f89de3795267e007008
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2018-11-09 21:18:28 -0400

    ätualizacao

--- matriz5_5/Program.cs
+++ matriz5_5/Program.cs
@@ -8,8 +8,8 @@ namespace matriz5_5
         {
             Console.WriteLine("Informe o numero a ser verificado: ");
             int numero = Int32.Parse(Console.ReadLine());
-
             int[,] matriz = new int[2, 2];
+            int contador = 0;
 
             for (int linha = 0; linha < 2; linha++)
             {  
@@ -17,21 +17,17 @@ namespace matriz5_5
                 {
                     Console.WriteLine("Informe um numero: ");
                     matriz[linha, coluna] = Int32.Parse(Console.ReadLine());
+                    if (numero == matriz[linha, coluna]) contador ++;
                 }
             }
-
-
-            int contador = 0; 
-            for (int linha = 0; linha < 2; linha++)
+            /*for (int linha = 0; linha < 2; linha++)
             {
                 for (int coluna = 0; coluna < 2; coluna++)
                 {
                     if (numero == matriz[linha, coluna]) contador++;
                 }
-            }
+            }*/
             Console.WriteLine("O numero {0} repetiu {1} vezes", numero, contador);
-
-
         }
     }
 }
